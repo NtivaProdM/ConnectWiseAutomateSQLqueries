e150846e3be4d5d2f0708d42c788137150e65392
Patrick <mcgannp@ntiva.com>
2017-03-21 13:40:44 -0400

    Update GetServersDriveInfo.sql

--- GetServersDriveInfo.sql
+++ GetServersDriveInfo.sql
@@ -10,27 +10,21 @@
  */
 
 SELECT
-  clients.name AS 'Client',
-  computers.name,
-  COUNT(DISTINCT computers.computerid) AS 'Total Servers',
-  ROUND(((SUM(drives.size))/1024),0) AS 'Total Drive Size GB',
-  ROUND(((SUM(drives.free))/1024),0) AS 'Total Drive free GB',
-  ROUND(((SUM(drives.size-drives.free))/1024),0) AS 'Total Drive usage GB'
-
+  clients.name AS 'Client'
+  , computers.name
+  , COUNT(DISTINCT computers.computerid) AS 'Total Servers'
+  ,  ROUND(((SUM(drives.size))/1024),0) AS 'Total Drive Size GB'
+  ,  ROUND(((SUM(drives.free))/1024),0) AS 'Total Drive free GB'
+  ,  ROUND(((SUM(drives.size-drives.free))/1024),0) AS 'Total Drive usage GB'
 FROM
      computers
-
 LEFT OUTER JOIN
      clients ON computers.clientid = clients.clientid
 LEFT OUTER JOIN
      drives ON computers.ComputerID = drives.computerid
-
 WHERE
      computers.os LIKE '%server%'
      AND drives.filesystem REGEXP 'ntfs|refs'
- 
  GROUP BY
-     computers.clientID,
-     computers.name;
-     
-     
+     computers.clientID
+     , computers.name;         
