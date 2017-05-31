1d0ba451bad0ee8595db6e0af6453ff58a1a5ff8
Patrick <mcgannp@ntiva.com>
2017-05-30 23:10:03 -0400

    Update GetWRClientsWithoutAutoDeploy.sql

--- GetWRClientsWithoutAutoDeploy.sql
+++ GetWRClientsWithoutAutoDeploy.sql
@@ -1,3 +1,8 @@
-SELECT clients.`Name`, plugin_webroot_clientsettings.`ClientID`, plugin_webroot_clientsettings.`Keycode`, plugin_webroot_clientsettings.`autoDeploy` FROM plugin_webroot_clientsettings LEFT JOIN clients ON clients.`ClientID` = plugin_webroot_clientsettings.`ClientID`
+SELECT clients.`Name`,
+plugin_webroot_clientsettings.`ClientID`,
+plugin_webroot_clientsettings.`Keycode`,
+plugin_webroot_clientsettings.`autoDeploy`
+FROM plugin_webroot_clientsettings 
+LEFT JOIN clients ON clients.`ClientID` = plugin_webroot_clientsettings.`ClientID`
 WHERE plugin_webroot_clientsettings.`autoDeploy` = 0;
 
