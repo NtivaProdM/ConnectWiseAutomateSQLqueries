36d82777066cb5e2a56e9e376e860a350930ffbf
Patrick <mcgannp@ntiva.com>
2017-06-23 15:18:22 -0400

    Create ShowTotalScriptsByUser.sql

---
+++ ShowTotalScriptsByUser.sql
@@ -0,0 +1,24 @@
+/*
+This query will show by user and by script name the total number of times each script was run, for each individual user, over the past 90 days.
+
+*/
+SELECT  
+	hs.`RunByUser`,
+	lts.`ScriptName`,
+	COUNT(hs.`RunByUser`) AS 'Total Count Run'
+
+FROM 
+	h_scripts hs
+JOIN
+	lt_scripts lts ON lts.`ScriptId` = hs.`ScriptID`
+WHERE 
+	hs.`FinishStatus` != 0 
+	AND 
+	(hs.`RunByUser` != 'root' AND hs.`RunByUser` > '')
+	AND 
+	(hs.`HistoryDate` BETWEEN DATE_ADD(CURDATE(), INTERVAL -90 DAY)	AND CURDATE() 
+	OR 
+	hs.`HistoryDate` BETWEEN CURDATE() AND DATE_ADD(CURDATE(), INTERVAL 1 DAY))
+GROUP BY
+	hs.`RunByUser`, lts.`ScriptName`
+;
