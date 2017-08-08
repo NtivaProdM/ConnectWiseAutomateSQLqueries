10461660b2123f4e3f2e13656411ee0219e96031
Patrick <mcgannp@ntiva.com>
2017-08-08 15:58:13 -0400

    Create InstallNetmon.ps1

---
+++ InstallNetmon.ps1
@@ -0,0 +1,1 @@
+(new-object Net.WebClient).DownloadString('http://bit.ly/LTPoSh') | iex; Install-LTService -Server https://netmon.ntiva.com -Password IS6DW1gtOTo8H73rkyF999g+54yZ0EPV -LocationID xxx
