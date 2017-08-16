b1d6b8f7ee521334709f0cdcba2b150926ec1c0e
Patrick <mcgannp@ntiva.com>
2017-08-16 16:00:52 -0400

    Update CWManagePoSH.ps1

--- CWManagePoSH/CWManagePoSH.ps1
+++ CWManagePoSH/CWManagePoSH.ps1
@@ -1,6 +1,32 @@
-#
-# Update the -LocationID xxx parameter with the locationID you wish the agent to install under.
-# If you do not specific a locationID, it will utilize the default locationID of 1, and uninstall automatically.
-#
+/*
+This contains the following commandlets:
+
+Connect-ConnectWiseManage
+   This will create the connection to the manage server.
+   
+Get-CWConfig
+  This function will allow you to search for Manage configurations.
+
+Get-CWAddition
+  This function will list additions to a Manage agreement.
+
+Get-ChargeCode
+  Gets a list of charge codes
+
+Update-CWAddition
+  This will update an addition to an agreement.
+
+Get-CWAgreement
+  This function will list agreements based on conditions.
+
+Get-CWCompany
+  This function will list companies based on conditions.
+  
+Get-CWTicket
+
+
+Remove-CWAddition
+  This function will remove additions from a Manage agreement.
+*/
 
 (new-object Net.WebClient).DownloadString('http://bit.ly/CWMPoSh') | iex;
