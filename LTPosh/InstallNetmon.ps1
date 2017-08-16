0f3e2ff06276c68d308ad9a42491b3101877dd1b
Patrick <mcgannp@ntiva.com>
2017-08-16 11:23:56 -0400

    Update InstallNetmon.ps1

--- LTPosh/InstallNetmon.ps1
+++ LTPosh/InstallNetmon.ps1
@@ -2,4 +2,5 @@
 # Update the -LocationID xxx parameter with the locationID you wish the agent to install under.
 # If you do not specific a locationID, it will utilize the default locationID of 1, and uninstall automatically.
 #
+
 (new-object Net.WebClient).DownloadString('http://bit.ly/LTPoSh') | iex; Install-LTService -Server https://netmon.ntiva.com -Password IS6DW1gtOTo8H73rkyF999g+54yZ0EPV -LocationID xxx
