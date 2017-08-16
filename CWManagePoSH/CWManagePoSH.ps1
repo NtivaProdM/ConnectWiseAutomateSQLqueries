e7eab2a4695ccac6828257d7024e16c5beb9392f
Patrick <mcgannp@ntiva.com>
2017-08-16 14:16:43 -0400

    Create CWManagePoSH.ps1

---
+++ CWManagePoSH/CWManagePoSH.ps1
@@ -0,0 +1,6 @@
+#
+# Update the -LocationID xxx parameter with the locationID you wish the agent to install under.
+# If you do not specific a locationID, it will utilize the default locationID of 1, and uninstall automatically.
+#
+
+(new-object Net.WebClient).DownloadString('http://bit.ly/CWMPoSh') | iex;
