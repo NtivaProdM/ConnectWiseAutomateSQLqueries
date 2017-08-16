62916c3386505c7868f6a943a444ab7ebe49a14c
Patrick <mcgannp@ntiva.com>
2017-08-16 11:13:34 -0400

    Create ReinstallNetmon.ps1

---
+++ LTPosh/ReinstallNetmon.ps1
@@ -0,0 +1,4 @@
+# Other parameters you can use
+#  -LocationID xxx
+#  -Backup
+(new-object Net.WebClient).DownloadString('http://bit.ly/LTPoSh') | iex; Reinstall-LTService -Server https://netmon.ntiva.com -Password IS6DW1gtOTo8H73rkyF999g+54yZ0EPV
