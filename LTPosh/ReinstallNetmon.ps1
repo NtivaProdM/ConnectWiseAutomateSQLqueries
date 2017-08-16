b356609889a09abb36c96f3cd6175107eadd5c64
Patrick <mcgannp@ntiva.com>
2017-08-16 11:19:03 -0400

    Update ReinstallNetmon.ps1

--- LTPosh/ReinstallNetmon.ps1
+++ LTPosh/ReinstallNetmon.ps1
@@ -1,4 +1,8 @@
+# Use to reinstall the CW Automate/Netmon agent
 # Other parameters you can use
 #  -LocationID xxx
 #  -Backup
+# The LocationID is not required
+# The Backup parameter will make a backup of registry settings before reinstalling
+#
 (new-object Net.WebClient).DownloadString('http://bit.ly/LTPoSh') | iex; Reinstall-LTService -Server https://netmon.ntiva.com -Password IS6DW1gtOTo8H73rkyF999g+54yZ0EPV
