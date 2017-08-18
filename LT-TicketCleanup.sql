d72ec5f458870a7792218875124ca7cc698846d5
Patrick <mcgannp@ntiva.com>
2017-08-18 08:42:53 -0400

    Update LT-TicketCleanup.sql

--- LT-TicketCleanup.sql
+++ LT-TicketCleanup.sql
@@ -1,6 +1,8 @@
 #Delete tickets for computers that are no longer in LT
 SELECT COUNT(*) FROM `tickets` WHERE ComputerID NOT IN (SELECT ComputerID FROM computers);
 DELETE FROM `tickets` WHERE ComputerID NOT IN (SELECT ComputerID FROM computers);
+
+#You should run these two again after completing the other steps
 #Delete ticket data for tickets that have been deleted.
 SELECT COUNT(*) FROM `ticketdata` WHERE TicketID NOT IN (SELECT TicketID FROM tickets);
 DELETE FROM `ticketdata` WHERE TicketID NOT IN (SELECT TicketID FROM tickets);
@@ -12,12 +14,15 @@ DELETE FROM ScriptState WHERE LOWER(Variable) LIKE '%ticketid' AND VALUE NOT IN 
 SELECT COUNT(*) FROM `tickets` WHERE STATUS=4 AND UpdateDate < DATE_SUB(NOW(), INTERVAL 15 DAY);
 #DELETE FROM `tickets` WHERE STATUS=4 AND UpdateDate < DATE_SUB(NOW(), INTERVAL 15 DAY);
 
+#Delete unsynced tickets that are older than 10 days
+SELECT COUNT(*) FROM `tickets` WHERE ExternalID=0 AND UpdateDate < DATE_SUB(NOW(), INTERVAL 10 DAY);
+DELETE FROM `tickets` WHERE ExternalID=0 AND UpdateDate < DATE_SUB(NOW(), INTERVAL 10 DAY);
+
 #Delete all tickets older than 1 year
 SELECT COUNT(*) FROM `tickets` WHERE UpdateDate < DATE_SUB(NOW(), INTERVAL 1 YEAR);
-#DELETE FROM `tickets` WHERE UpdateDate < DATE_SUB(NOW(), INTERVAL 1 YEAR);
+DELETE FROM `tickets` WHERE UpdateDate < DATE_SUB(NOW(), INTERVAL 1 YEAR);
 
 #Delete informational tickets with attachements that are older than one day.
 SELECT COUNT(*) FROM Tickets WHERE SUBJECT REGEXP 'AD Schema Report for|GPO backup and Reporting for|DELETE ME -|Exchange Database Report|Exchange Server Email Stats|Exchange Server Reporting for|Exchange Server Best Practice Report for|Windows 2008 R2 Best Practice Analyzer for|RBL Check found' AND startedDate > DATE_SUB(NOW(), INTERVAL 1 DAY);
 #DELETE FROM Tickets WHERE SUBJECT REGEXP 'AD Schema Report for|GPO backup and Reporting for|DELETE ME -|Exchange Database Report|Exchange Server Email Stats|Exchange Server Reporting for|Exchange Server Best Practice Report for|Windows 2008 R2 Best Practice Analyzer for|RBL Check found' AND startedDate > DATE_SUB(NOW(), INTERVAL 1 DAY);
-
 
